2319d5218cee349a4bc486dafb07258af6a78e31
Pachara Chantawong <66879660+yukikwi@users.noreply.github.com>
2021-10-04 18:10:08 +0700

    [WIP] Add databaseConnection
    this need to test more.

---
+++ database/connection.ts
@@ -0,0 +1,45 @@
+/* database/connection.ts
+*
+* This does a create connection to mongoose database.
+* 
+* Created by Pachara Chantawong, 04 October 2021 as part of the SE COVID BED
+*
+* Revision 1.0 2021/10/04 17:54:00 pachara
+* initial DatabaseConnection with method getConnection: Boolean
+*/
+
+// load mongoose dependency
+import mongoose from "mongoose"
+// load dotenv for load environment variable from .env file
+require('dotenv').config()
+
+export default class DatabaseConnection {
+    // class variable
+    mongoDbUri:string
+
+    constructor () {
+        if (process.env.MONGODB_URI){
+            this.mongoDbUri = process.env.MONGODB_URI
+        }
+        else{
+            // if not specific MONGODB_URI then throw error.
+            throw new Error(
+                'Please define the MONGODB_URI environment variable inside .env'
+            )
+        }
+    }
+
+    // get database connection
+    async connectDatabase () {
+        // try connect to mongodb
+        try{
+            await mongoose.connect(this.mongoDbUri)
+            return true
+        }
+        catch(e){
+            console.log('mongoose: connection failed')
+            console.log(e)
+            return false
+        }
+    }
+}
---
+++ database/model/README.md
@@ -0,0 +1,43 @@
+
+**This folder is for mongodb interface and document model**
+
+  
+
+> Example
+> 
+>     import mongoose, { Document, model, Model, Schema } from "mongoose"
+>     
+>       
+>     
+>     export interface ..name..Interface extends Document {
+>     
+>     variable1: datatype
+>     
+>     variable2: string
+>     
+>     }
+>     
+>       
+>     
+>     const ..name..Schema: Schema = new Schema({
+>     
+>     variable1: {
+>     
+>     type: datatype
+>     
+>     },
+>     
+>     reference variable2: {
+>     
+>     type: Schema.Types.ObjectId,
+>     
+>     ref: reference collection
+>     
+>     }
+>     
+>     })
+>     
+>       
+>       
+>     
+>     export const Post: Model<..name..Interface> = mongoose.models...name.. || model("..name..", ..name..Schema)
