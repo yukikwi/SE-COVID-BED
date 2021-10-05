a5ea577e7089c528feb31ef12621aedeb18785f4
Pachara Chantawong <pachara2202@gmail.com>
2021-10-06 00:26:36 +0700

    [BBMN-17] Add custom className support to PositiveButton component

--- components/PositiveButton.tsx
+++ components/PositiveButton.tsx
@@ -2,19 +2,21 @@ import React, { ReactElement } from 'react'
 import { useRouter } from 'next/router'
 
 interface Props {
-    target: string
-    text: string
+  className: string,
+  target: string
+  text: string
 }
 
 function PositiveButton(props: Props): ReactElement {
-  const {target, text} = props
+  const {target, text, className} = props
   const router = useRouter()
   const goToTarget = () => {
     router.push(target)
   }
+  const componentClassName = `${className} tw-rounded-full tw-bg-dark-green tw-p-5 tw-text-white tw-w-3/5 md:tw-w-2/5 tw-text-2xl`
 
   return (
-    <button onClick={goToTarget} className="tw-rounded-full tw-bg-dark-green tw-p-5 tw-text-white tw-w-1/4 tw-text-2xl">
+    <button onClick={goToTarget} className={componentClassName}>
       {text}
     </button>
   )
