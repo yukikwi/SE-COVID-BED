7dfc7cc0b10dd69c920f328623e954959c4d4e6b
Pachara Chantawong <66879660+yukikwi@users.noreply.github.com>
2021-10-06 11:45:11 +0700

    [BBMN-18] Display container shadow on all resolution

--- pages/login.tsx
+++ pages/login.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
     <div className="tw-flex tw-flex-col tw-mx-auto tw-w-full md:tw-w-3/4 tw-min-h-screen tw-pt-5">
       <Logo />
       <div className="tw-relative tw-flex tw-flex-col tw-flex-grow">
-        <div className="tw-bg-white tw-flex tw-flex-grow tw-flex-col tw-rounded-t-3xl tw-p-5 md:tw-shadow-top-xl-dark-green">
+        <div className="tw-bg-white tw-flex tw-flex-grow tw-flex-col tw-rounded-t-3xl tw-p-5 tw-shadow-top-xl-dark-green">
           <div className="tw-flex tw-flex-row tw-justify-between tw-mb-10">
             <div className="tw-w-8 tw-h-8 tw-rounded-full tw-bg-dark-green" />
             <div className="tw-w-8 tw-h-8 tw-rounded-full tw-bg-dark-green" />
