101ee6f7446acc4bcfebffc08473ed676b2d6499
Pachara Chantawong <66879660+yukikwi@users.noreply.github.com>
2021-10-06 09:02:47 +0700

    [BBMN-18] Remove duplicate color and add custom boxShadow

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -8,12 +8,12 @@ function Logo({}: Props): ReactElement {
   return (
     <div className="tw-mb-5">
       <div className="tw-relative tw-h-14 md:tw-h-24 tw-text-center">
-        <h1 className="tw-absolute tw-font-bold md:tw-text-8xl tw-text-5xl tw-text-dark-matcha-green-medium tw-w-full tw-z-10">COVID-19</h1>
-        <h1 className="tw-absolute tw-font-bold md:tw-text-8xl tw-text-5xl tw-text-dark-matcha-green-dark tw-top-0.5 tw-left-0.5 tw-w-full tw-z-0">COVID-19</h1>
+        <h1 className="tw-absolute tw-font-bold md:tw-text-8xl tw-text-5xl tw-text-dark-matcha-green tw-w-full tw-z-10">COVID-19</h1>
+        <h1 className="tw-absolute tw-font-bold md:tw-text-8xl tw-text-5xl tw-text-dark-green tw-top-0.5 tw-left-0.5 tw-w-full tw-z-0">COVID-19</h1>
       </div>
       <div className="tw-relative tw-h-14 tw-text-center">
-        <h3 className="tw-absolute tw-font-bold tw-text-2xl md:tw-text-3xl tw-text-dark-matcha-green-medium tw-w-full tw-z-10">Manage hospital resouce</h3>
-        <h3 className="tw-absolute tw-font-bold tw-text-2xl md:tw-text-3xl tw-text-dark-matcha-green-dark tw-top-0.5 tw-left-0.5 tw-w-full tw-z-0">Manage hospital resouce</h3>
+        <h3 className="tw-absolute tw-font-bold tw-text-2xl md:tw-text-3xl tw-text-dark-matcha-green tw-w-full tw-z-10">Manage hospital resouce</h3>
+        <h3 className="tw-absolute tw-font-bold tw-text-2xl md:tw-text-3xl tw-text-dark-green tw-top-0.5 tw-left-0.5 tw-w-full tw-z-0">Manage hospital resouce</h3>
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,10 @@ module.exports = {
       colors: {
         'dark-green': '#354F52',
         'matcha-green': '#CAD2C5',
-        'dark-matcha-green': {
-          medium: '#84A98C',
-          dark: '#354F52'
-        }
+        'dark-matcha-green': '#84A98C'
+      },
+      boxShadow: {
+        'top-xl-dark-green': '0 -20px 0 0 #354F52'
       }
     },
   },
