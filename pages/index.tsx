3d33498db8aa810f4aa5d64d0f64091251f8bcbc
Pachara Chantawong <66879660+yukikwi@users.noreply.github.com>
2021-10-06 09:11:33 +0700

    [BBMN-18] Improve content size on mobile for homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,11 @@ import PositiveButton from '../components/PositiveButton'
 
 const Home: NextPage = () => {
   return (
-    <div className="tw-mx-auto tw-w-4/5 md:tw-w-1/2 tw-fixed tw-top-1/2 tw-left-1/2 tw-transform tw--translate-x-2/4 tw--translate-y-2/4">
+    <div className="tw-mx-auto tw-w-4/5 lg:tw-w-1/2 tw-fixed tw-top-1/2 tw-left-1/2 tw-transform tw--translate-x-2/4 tw--translate-y-2/4">
       <Logo />
       <div className="tw-flex tw-justify-center tw-flex-col md:tw-flex-row">
-        <PositiveButton className="md:tw-mr-5 tw-w-full md:tw-w-auto" target="/" text="Patient" />
-        <PositiveButton className="tw-mt-3 md:tw-mt-0 tw-w-full md:tw-w-auto" target="/login" text="Staff" />
+        <PositiveButton className="md:tw-mr-5 tw-w-full md:tw-w-2/5" target="/" text="Patient" />
+        <PositiveButton className="tw-mt-3 md:tw-mt-0 tw-w-full md:tw-w-2/5" target="/login" text="Staff" />
       </div>
     </div>
   )
