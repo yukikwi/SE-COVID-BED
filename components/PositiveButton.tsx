af8db92cf8ed63d96a69ca74cff51a422a214f5f
Pachara Chantawong <66879660+yukikwi@users.noreply.github.com>
2021-10-06 08:25:42 +0700

    [BBMN-18] Make PositiveButton component support type

--- components/PositiveButton.tsx
+++ components/PositiveButton.tsx
@@ -3,12 +3,13 @@ import { useRouter } from 'next/router'
 
 interface Props {
   className: string,
-  target: string
-  text: string
+  target: string,
+  text: string,
+  htmlType: "button" | "submit" | "reset" | undefined
 }
 
 function PositiveButton(props: Props): ReactElement {
-  const {target, text, className} = props
+  const {target, text, className, htmlType} = props
   const router = useRouter()
   const goToTarget = () => {
     router.push(target)
@@ -16,10 +17,17 @@ function PositiveButton(props: Props): ReactElement {
   const componentClassName = `${className} tw-rounded-full tw-bg-dark-green tw-p-5 tw-text-white tw-w-3/5 md:tw-w-2/5 tw-text-2xl`
 
   return (
-    <button onClick={goToTarget} className={componentClassName}>
+    <button onClick={goToTarget} className={componentClassName} type={htmlType}>
       {text}
     </button>
   )
 }
+
+PositiveButton.defaultProps = {
+  className: '',
+  target: '',
+  text: '',
+  htmlType: 'button'
+};
 
 export default PositiveButton
