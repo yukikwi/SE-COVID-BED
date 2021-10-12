b876280768067f6da077983b4e6b8c5986c01f47
Pachara Chantawong <66879660+yukikwi@users.noreply.github.com>
2021-10-12 20:00:12 +0700

    [BBMN-29] Create hospital resources list page

---
+++ pages/hospital/resources/index.tsx
@@ -0,0 +1,76 @@
+import type { NextPage } from 'next'
+import LayoutHospital from '../../../components/Layout/Hospital'
+import { Table } from 'antd';
+import Status from '../../../components/Hospital/Status'
+import { EyeOutlined, EditOutlined, DeleteOutlined } from '@ant-design/icons'
+
+type TResource = {
+  key: string,
+  resource: string,
+  amount: number,
+  avaliable: number
+}
+
+const HospitalResourceIndex: NextPage = () => {
+  // Dummy Hospital data
+  const columns = [
+    {
+      title: 'Resource',
+      dataIndex: 'resource',
+      key: 'resource'
+    },
+    {
+      title: 'Maximum',
+      dataIndex: 'amount',
+      key: 'amount',
+    },
+    {
+      title: 'Avaliable',
+      dataIndex: 'avaliable',
+      key: 'avaliable',
+    },
+    {
+      title: 'Status',
+      key: 'status',
+      render: (record:TResource) => (
+        <Status avaliable={record.avaliable} amount={record.amount} />
+      )
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (record:TResource) => (
+        <div>
+          <a className="hover:tw-text-green-500" href="#"><EyeOutlined className="tw-font-base tw-mr-3" /></a>
+          <a className="hover:tw-text-yellow-500" href="#"><EditOutlined className="tw-font-base tw-mr-3" /></a>
+          <a className="hover:tw-text-red-500" href="#"><DeleteOutlined className="tw-font-base tw-mr-3" /></a>
+        </div>
+      )
+    }
+  ];
+
+  const data = [
+    {
+      key: '1',
+      resource: 'bed',
+      amount: 32,
+      avaliable: 32
+    },
+    {
+      key: '2',
+      resource: 'Respirator',
+      amount: 32,
+      avaliable: 32
+    }
+  ];
+
+  return (
+    <LayoutHospital title="Capybara Hospital : Resource list">
+      <div className="tw-overflow-x-scroll">
+        <Table columns={columns} dataSource={data} />
+      </div>
+    </LayoutHospital>
+  )
+}
+
+export default HospitalResourceIndex
