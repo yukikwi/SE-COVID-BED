5cbc96c24dd344afbadf61c4916bc65fed538254
Thanawat Spp <thanawat.spp@gmail.com>
2021-10-14 20:41:04 +0700

    bk

--- pages/system/index.tsx
+++ pages/system/index.tsx
@@ -1,28 +1,33 @@
-import type { NextPage } from 'next'
-import axios, { AxiosError } from 'axios'
-import LayoutHospital from '../../components/Layout/Hospital'
-import ModalDelete from '../../components/System/ModalDelete'
-import ModalAddEdit from '../../components/System/ModalAddEdit'
-import { Table, Button, notification } from 'antd';
-import Status from '../../components/System/Status'
-import { EyeOutlined, EditOutlined, DeleteOutlined, PlusSquareOutlined } from '@ant-design/icons'
+import type { NextPage } from "next";
+import axios, { AxiosError } from "axios";
+import LayoutHospital from "../../components/Layout/Hospital";
+import ModalDelete from "../../components/System/ModalDelete";
+import ModalAddEdit from "../../components/System/ModalAddEdit";
+import { Table, Button, notification } from "antd";
+import Status from "../../components/System/Status";
+import {
+  EyeOutlined,
+  EditOutlined,
+  DeleteOutlined,
+  PlusSquareOutlined,
+} from "@ant-design/icons";
 import { useDispatch, useSelector } from "react-redux";
 import { showDeleteModal } from "../../store/deleteModal/actions";
 import { showAddOrEditModal } from "../../store/addOrEditModal/actions";
-import { IHospital } from '../../class/data_struct/hospital'
-import { useEffect, useState } from 'react'
-import { getDeleteModalState } from '../../store/deleteModal/selectors'
+import { IHospital } from "../../class/data_struct/hospital";
+import { useEffect, useState } from "react";
+import { getDeleteModalState } from "../../store/deleteModal/selectors";
 
 type TUiHospital = {
-  key: string,
-  _id: string,
-  hospital: string,
-  convince: string,
-  staff: string,
-  avaliable: number,
-  amount: number,
-  isClose: boolean
-}
+  key: string;
+  _id: string;
+  hospital: string;
+  convince: string;
+  staff: string;
+  avaliable: number;
+  amount: number;
+  isClose: boolean;
+};
 
 const HospitalResourceIndex: NextPage = () => {
   const [selectedHospital, setSelectedHospital] = useState("");
@@ -53,62 +58,96 @@ const HospitalResourceIndex: NextPage = () => {
       key: "avaliable",
     },
     {
-      title: 'Status',
-      key: 'status',
-      render: (record:TUiHospital) => (
-        <Status isClose={record.isClose} avaliable={record.avaliable} amount={record.amount} />
-      )
+      title: "Status",
+      key: "status",
+      render: (record: TUiHospital) => (
+        <Status
+          isClose={record.isClose}
+          avaliable={record.avaliable}
+          amount={record.amount}
+        />
+      ),
     },
     {
-      title: 'Action',
-      key: 'action',
-      render: (record:TUiHospital) => (
+      title: "Action",
+      key: "action",
+      render: (record: TUiHospital) => (
         <div>
-          <a className="hover:tw-text-green-500"><EyeOutlined className="tw-font-base tw-mr-3" /></a>
-          <a className="hover:tw-text-yellow-500" onClick={() => { dispatch(showAddOrEditModal('Edit')) }}><EditOutlined className="tw-font-base tw-mr-3" /></a>
-          <a className="hover:tw-text-red-500" onClick={() => { dispatch(showDeleteModal(record._id)) }}><DeleteOutlined className="tw-font-base tw-mr-3" /></a>
+          <a className="hover:tw-text-green-500">
+            <EyeOutlined className="tw-font-base tw-mr-3" />
+          </a>
+          <a
+            className="hover:tw-text-yellow-500"
+            onClick={() => {
+              dispatch(showAddOrEditModal("Edit"));
+            }}
+          >
+            <EditOutlined className="tw-font-base tw-mr-3" />
+          </a>
+          <a
+            className="hover:tw-text-red-500"
+            onClick={() => {
+              dispatch(showDeleteModal(record._id));
+            }}
+          >
+            <DeleteOutlined className="tw-font-base tw-mr-3" />
+          </a>
         </div>
       ),
     },
   ];
 
   // Fetch data from api
-  const [data, setData] = useState<Array<TUiHospital>>()
+  const [data, setData] = useState<Array<TUiHospital>>();
   const getHospitalsData = async () => {
-    let hospitalData:Array<TUiHospital> = []
-    try{
-      let apiResonse = await axios.get(`${process.env.NEXT_PUBLIC_APP_API}/hospital`)
-      let rawHospitalData:Array<IHospital> = apiResonse.data
-      for (let i = 0; i < rawHospitalData.length; i++){
+    let hospitalData: Array<TUiHospital> = [];
+    try {
+      let apiResonse = await axios.get(
+        `${process.env.NEXT_PUBLIC_APP_API}/hospital`
+      );
+      let rawHospitalData: Array<IHospital> = apiResonse.data;
+
+      const temp = rawHospitalData.map((hospital: IHospital) => ({
+        key: hospital._id,
+        hospital: hospital.hospitalName,
+        convince: hospital.hospitalConvince,
+        staff: "Dr. Dio",
+        amount: 32,
+        avaliable: 32,
+        isClose: hospital.isAvaliable,
+      }));
+      console.log("temp", temp);
+
+      for (let i = 0; i < rawHospitalData.length; i++) {
         hospitalData.push({
           key: i.toString(),
           _id: rawHospitalData[i]._id,
           hospital: rawHospitalData[i].hospitalName,
           convince: rawHospitalData[i].hospitalConvince,
-          staff: 'Dr.Dio',
+          staff: "Dr.Dio",
           amount: 32,
           avaliable: 32,
-          isClose: rawHospitalData[i].isAvaliable
-        })
+          isClose: rawHospitalData[i].isAvaliable,
+        });
       }
-      setData(hospitalData)
-    }
-    catch (error: any | AxiosError) {
-      console.log(error.response.status)
+      setData(hospitalData);
+    } catch (error: any | AxiosError) {
+      console.log(error.response.status);
       notification.open({
-        message: 'Error',
-        description: 'Cannot connect to api. Please contact admin for more information.'
+        message: "Error",
+        description:
+          "Cannot connect to api. Please contact admin for more information.",
       });
     }
-  }
+  };
 
   // Redux part
   const deleteModalState = useSelector(getDeleteModalState);
   const dispatch = useDispatch();
-  
+
   useEffect(() => {
-    getHospitalsData()
-  },[deleteModalState])
+    getHospitalsData();
+  }, [deleteModalState]);
 
   return (
     <LayoutHospital
