d21dd6b6e698b5b3da522e5ce7ccbd1f00d3f832
Pachara Chantawong <66879660+yukikwi@users.noreply.github.com>
2021-11-02 23:45:38 +0700

    [BBMN-88] add Map component to AddEdit hospital modal

--- components/System/ModalHospitalForm/AddEdit.tsx
+++ components/System/ModalHospitalForm/AddEdit.tsx
@@ -1,12 +1,15 @@
 import React, { ReactElement, useEffect, useState } from "react";
-import { Form, Input, Button, notification, Select } from "antd";
+import { Form, Input, Button, notification, Select, AutoComplete } from "antd";
 import HospitalStatus from "./HospitalStatus";
 import Resources from "./Resources";
 import { IHospital } from "../../../class/data_struct/hospital";
-import { initValue } from "../ModalAddEdit";
 import axios from "axios";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getAddOrEditModalState } from "../../../store/addOrEditHospitalModal/selectors";
+import { setLoc } from "../../../store/map/actions";
+import { TambonType } from '../../TamBonType'
+import tambon from '../../../public/location.json';
+import Map from '../../Map/Map'
 
 interface Props {
   hospitalData: IHospital;
@@ -14,18 +17,16 @@ interface Props {
   isShow: boolean;
 }
 
-interface IHospitalData {
-  hospitalName?: string;
-  hospitalAddress?: string;
-  staff?: string;
-  hospitalStatus?: "open" | "close";
-}
-
 function AddEditForm(props: Props): ReactElement {
   const { Option } = Select;
   const [dummyUser, setDummyUser] = useState<Array<any>>([]);
   const [addHospital, setAddHospital] = useState<any>({ _id: null });
-
+  const [options, setOptions] = useState<{ value: string, label: JSX.Element }[]>([]);
+  const dispatch = useDispatch();
+  const [preLat, setPreLat] = useState(0)
+  const [preLong, setPreLong] = useState(0)
+  const tambonData:Array<TambonType> | any = tambon["TAMBON"]
+  
   // UI - user list
   const setUserData = () => {
     setDummyUser([
@@ -51,7 +52,6 @@ function AddEditForm(props: Props): ReactElement {
     setUserData();
   }, []);
 
-  // let hospitalData: IHospitalData = { hospitalStatus: "open" };
   const { hospitalData, mode } = props;
   const [form] = Form.useForm();
   const { show } = useSelector(getAddOrEditModalState);
@@ -62,7 +62,7 @@ function AddEditForm(props: Props): ReactElement {
   // get Changed value
   const updateHospitalData = (changed: any) => {
     formData = { ...formData, ...changed };
-    console.log(formData);
+    console.log('updateHospitalData', formData);
   };
 
   // update status toggle value on change
@@ -121,6 +121,56 @@ function AddEditForm(props: Props): ReactElement {
     }
   };
 
+  // map handler
+  const onSearch = (searchText: string) => {
+    const tambonDataResult = tambonData.filter((item:TambonType) => {
+      return item.TAMBON_T.includes(searchText)
+    } )
+    
+    let tambonUI = []
+    for(let i = 0; i < tambonDataResult.length; i++){
+      tambonUI.push({
+        value: `${tambonDataResult[i].TAMBON_T} - ${tambonDataResult[i].AMPHOE_T} - ${tambonDataResult[i].CHANGWAT_T}`,
+        label: (
+          <div className="tw-flex tw-justify-between">
+            <span>
+              {tambonDataResult[i].TAMBON_T} - {tambonDataResult[i].AMPHOE_T} - {tambonDataResult[i].CHANGWAT_T}
+            </span>
+          </div>
+        ),
+      })
+    }
+    if(searchText.length >= 2){
+      setOptions(tambonUI);
+    }
+  };
+
+  const onSelect = (value: string) => {
+    const data = value.split(' - ')
+    console.log(data)
+    if(value != '-1'){
+      const tambonDataResult = tambonData.filter((x:any) => {
+        return x.TAMBON_T === data[0] && x.AMPHOE_T === data[1] && x.CHANGWAT_T === data[2]
+      })
+      console.log(tambonDataResult)
+      form.setFieldsValue({
+        subdistrict: data[0],
+        district: data[1],
+        province: data[2]
+      })
+
+      // update Map loc
+      console.log('Update prop')
+      dispatch(setLoc({
+        lat: Number(tambonDataResult[0].LAT),
+        long: Number(tambonDataResult[0].LONG),
+      }))
+      setPreLat(Number(tambonDataResult[0].LAT))
+      setPreLong(Number(tambonDataResult[0].LONG))
+    }
+  };
+  // end map handler
+
   useEffect(() => {
     setAddHospital({ _id: null });
     form.resetFields();
@@ -163,6 +213,40 @@ function AddEditForm(props: Props): ReactElement {
       </Form.Item>
 
       <Form.Item
+          label="Sub District"
+          name="subdistrict"
+          rules={[
+            { required: true, message: "Please input your sub district!" },
+          ]}
+        >
+          <AutoComplete
+            options={options}
+            onSearch={onSearch}
+            onSelect={onSelect}
+          />
+        </Form.Item>
+
+        <Form.Item
+          label="District"
+          name="district"
+          rules={[
+            { required: true, message: "Please input your district!" },
+          ]}
+        >
+          <Input disabled />
+        </Form.Item>
+
+        <Form.Item
+          label="Province"
+          name="province"
+          rules={[
+            { required: true, message: "Please input your province!" },
+          ]}
+        >
+          <Input disabled />
+        </Form.Item>
+
+      <Form.Item
         label="Hospital convince"
         name="hospitalConvince"
         rules={[{ required: true, message: "Please input Hospital convince!" }]}
@@ -197,8 +281,11 @@ function AddEditForm(props: Props): ReactElement {
         />
       </Form.Item>
 
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+      <Map preLat={preLat} preLong={preLong} />
+
+      <Form.Item className="tw-my-5" wrapperCol={{ span: 24 }}>
         <Button
+          className="tw-w-full"
           type="primary"
           htmlType="submit"
           disabled={mode === "Add" && addHospital._id ? true : false}
