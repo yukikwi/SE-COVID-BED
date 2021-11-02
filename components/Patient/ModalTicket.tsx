8a53310a36d800bd7de5ab2beb2e15c9107e2035
Pachara Chantawong <66879660+yukikwi@users.noreply.github.com>
2021-11-02 23:46:29 +0700

    [BBMN-88] support duplicate subdistrict but different district and province

--- components/Patient/ModalTicket.tsx
+++ components/Patient/ModalTicket.tsx
@@ -30,7 +30,7 @@ function ModalTicket({}: Props): ReactElement {
   const dispatch = useDispatch();
   const show = useSelector(getTicketModalState);
   const [form] = Form.useForm();
-  const [options, setOptions] = useState<{ value: string }[]>([]);
+  const [options, setOptions] = useState<{ value: string, label: JSX.Element }[]>([]);
   const tambonData:Array<TambonType> | any = tambon["TAMBON"]
   const loc = useSelector(getMapState)
   const [preLat, setPreLat] = useState(0)
@@ -49,53 +49,63 @@ function ModalTicket({}: Props): ReactElement {
     dispatch(hideTicketModal())
   }
 
-  const addrToLoc = async () => {
-    const address = form.getFieldValue('address')
-    if(address !== ''){
-      const tambonDataa = tambonData.filter((item:TambonType) => {
-        return item.TAMBON_T.includes('บางขวัญ')
-      } )
-      console.log('tambonDataa', tambonDataa)
-    }
-    
-  }
-
   const onSearch = (searchText: string) => {
     const tambonDataResult = tambonData.filter((item:TambonType) => {
       return item.TAMBON_T.includes(searchText)
     } )
-    const tambonOnlyData = tambonDataResult.map((x:TambonType) => x.TAMBON_T)
+    
     let tambonUI = []
-    for(let i = 0; i < tambonOnlyData.length; i++){
+    for(let i = 0; i < tambonDataResult.length; i++){
       tambonUI.push({
-        value: tambonOnlyData[i]
+        value: `${tambonDataResult[i].TAMBON_T} - ${tambonDataResult[i].AMPHOE_T} - ${tambonDataResult[i].CHANGWAT_T}`,
+        label: (
+          <div className="tw-flex tw-justify-between">
+            <span>
+              {tambonDataResult[i].TAMBON_T} - {tambonDataResult[i].AMPHOE_T} - {tambonDataResult[i].CHANGWAT_T}
+            </span>
+          </div>
+        ),
       })
     }
-    console.log(tambonUI)
     if(searchText.length >= 2){
       setOptions(
-        !searchText ? [] : tambonUI,
+        !searchText ? [{
+          value: '-1',
+          label: (
+            <div className="tw-flex tw-justify-between">
+            <span>
+              Please enter more than 2 characters.
+            </span>
+          </div>
+          )
+        }] : tambonUI,
       );
     }
   };
 
-  const onSelect = (data: string) => {
-    const tambonDataResult = tambonData.filter((item:TambonType) => {
-      return item.TAMBON_T.includes(data)
-    } )
-    form.setFieldsValue({
-      district: tambonDataResult[0].AMPHOE_T,
-      province: tambonDataResult[0].CHANGWAT_T
-    })
+  const onSelect = (value: string) => {
+    const data = value.split(' - ')
+    console.log(data)
+    if(value != '-1'){
+      const tambonDataResult = tambonData.filter((x:any) => {
+        return x.TAMBON_T === data[0] && x.AMPHOE_T === data[1] && x.CHANGWAT_T === data[2]
+      })
+      console.log(tambonDataResult)
+      form.setFieldsValue({
+        subdistrict: data[0],
+        district: data[1],
+        province: data[2]
+      })
 
-    // update Map loc
-    console.log('Update prop')
-    dispatch(setLoc({
-      lat: Number(tambonDataResult[0].LAT),
-      long: Number(tambonDataResult[0].LONG),
-    }))
-    setPreLat(Number(tambonDataResult[0].LAT))
-    setPreLong(Number(tambonDataResult[0].LONG))
+      // update Map loc
+      console.log('Update prop')
+      dispatch(setLoc({
+        lat: Number(tambonDataResult[0].LAT),
+        long: Number(tambonDataResult[0].LONG),
+      }))
+      setPreLat(Number(tambonDataResult[0].LAT))
+      setPreLong(Number(tambonDataResult[0].LONG))
+    }
   };
 
   return (
@@ -108,9 +118,6 @@ function ModalTicket({}: Props): ReactElement {
         [
           <Button key="back" onClick={handleCancel}>
             Close
-          </Button>,,
-          <Button type="default" onClick={() => {addrToLoc()}}>
-            Get location from address
           </Button>,
           <Button key="submit" type="primary" onClick={() => {form.submit()}}>
             Save
