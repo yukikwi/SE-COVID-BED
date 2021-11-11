9c93214b7dfbf8c8c8016f0ebf6866030171c27f
Scherben <56989629+warn192444@users.noreply.github.com>
2021-11-11 22:44:25 +0700

    fix text and layout ui

--- components/Layout/Hospital.tsx
+++ components/Layout/Hospital.tsx
@@ -39,15 +39,15 @@ function LayoutHospital(props: Props): ReactElement {
   return (
     <div>
       <HospitalNavbar />
-      <div className="md:tw-mx-10 tw-mx-2 tw-my-5 tw-grid tw-grid-cols-2">
-        <div className="tw-col-span-2 md:tw-col-span-1">
+      <div className="md:tw-mx-10 tw-mx-2 tw-my-5 tw-grid tw-grid-cols-3">
+        <div className="tw-col-span-3 md:tw-col-span-2">
           <h1 className="tw-text-charcoal tw-text-3xl tw-font-bold">{ title }</h1>
         </div>
-        <div className="tw-col-span-2 md:tw-col-span-1 tw-mt-5 md:tw-mt-0">
+        <div className="tw-col-span-3 md:tw-col-span-1 tw-mt-5 md:tw-mt-0">
           { button }
         </div>
 
-        <div className="tw-bg-white tw-p-5 tw-mt-5 tw-col-span-2 tw-rounded-lg">
+        <div className="tw-bg-white tw-p-5 tw-mt-5 tw-col-span-3 tw-rounded-lg">
           { children }
         </div>
       </div>
