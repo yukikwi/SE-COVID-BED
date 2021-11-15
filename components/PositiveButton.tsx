200f395fefc31bb3ab11298af94dd57bd4a132e0
Pachara Chantawong <66879660+yukikwi@users.noreply.github.com>
2021-11-15 21:43:38 +0700

    [BBMN-111] Refactor /login

--- components/PositiveButton.tsx
+++ components/PositiveButton.tsx
@@ -1,30 +1,11 @@
 import React, { ReactElement } from "react";
-import { useRouter } from "next/router";
 
 interface Props {
+  id: string;
   className: string;
   text: string;
   onClick: () => void;
   htmlType: "button" | "submit" | "reset" | undefined;
-}
-
-function PositiveButton(props: Props): ReactElement {
-  const { onClick, text, className, htmlType } = props;
-  const router = useRouter();
-  const handleClick = () => {
-    onClick();
-  };
-  const componentClassName = `${className} tw-rounded-full tw-bg-dark-green tw-p-5 tw-text-white tw-min-w-3/5 md:tw-min-w-2/5 tw-text-2xl`;
-
-  return (
-    <button
-      onClick={handleClick}
-      className={componentClassName}
-      type={htmlType}
-    >
-      {text}
-    </button>
-  );
 }
 
 PositiveButton.defaultProps = {
@@ -32,5 +13,23 @@ PositiveButton.defaultProps = {
   text: "",
   htmlType: "button",
 };
+
+function PositiveButton(props: Props): ReactElement {
+  const { id, onClick, text, className, htmlType } = props;
+
+  // merge custom class with button class
+  const componentClassName = `${className} tw-rounded-full tw-bg-dark-green tw-p-5 tw-text-white tw-min-w-3/5 md:tw-min-w-2/5 tw-text-2xl`;
+
+  return (
+    <button
+      id={id}
+      onClick={onClick}
+      className={componentClassName}
+      type={htmlType}
+    >
+      {text}
+    </button>
+  );
+}
 
 export default PositiveButton;
